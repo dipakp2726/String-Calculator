340f547b812127a199bbfa710d261ae2518173f6
Dipak Prajapati <dipakp2726@gmail.com>
2025-02-19 17:07:07 +0530

    test: add test for supporting custom delimiters

--- test/src/string_calculator_test.dart
+++ test/src/string_calculator_test.dart
@@ -34,5 +34,10 @@ void main() {
       expect(calculator.add('1\n2,3'), equals(6));
       expect(calculator.add('1,2\n3'), equals(6));
     });
+
+    test('should support custom delimiters', () {
+      expect(calculator.add('//;\n1;2'), equals(3));
+      expect(calculator.add('//|\n1|2|3'), equals(6));
+    });
   });
 }
