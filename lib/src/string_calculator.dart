4866781c45e98f82b7595f8b1ba3edaf2b4b1464
Dipak Prajapati <dipakp2726@gmail.com>
2025-02-19 16:58:13 +0530

    feat: implement addition for comma-separated numbers

--- lib/src/string_calculator.dart
+++ lib/src/string_calculator.dart
@@ -7,10 +7,18 @@ class StringCalculator {
 
   /// Adds the numbers in the given string.
   ///
+  /// The string can contain numbers separated by commas.If the string is empty,
+  /// the method returns 0. If the string contains a single number,
+  /// it returns that number.
+  ///
   /// @param numbers The string containing numbers separated by commas.
   /// @return The sum of the numbers in the string.
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
-    return int.parse(numbers);
+    if (!numbers.contains(',')) return int.parse(numbers);
+
+    return numbers.split(',').map(int.parse).reduce(
+          (a, b) => a + b,
+        );
   }
 }
