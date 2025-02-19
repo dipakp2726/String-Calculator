a2af13e4b841cdcff224c7d7eedd5366eabbcff8
Dipak Prajapati <dipakp2726@gmail.com>
2025-02-19 17:02:37 +0530

    feat: Implemented splitting and summing of numbers - Refactored to use map and reduce for cleaner code

--- lib/src/string_calculator.dart
+++ lib/src/string_calculator.dart
@@ -15,9 +15,13 @@ class StringCalculator {
   /// @return The sum of the numbers in the string.
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
-    if (!numbers.contains(',')) return int.parse(numbers);
 
-    return numbers.split(',').map(int.parse).reduce(
+    return numbers
+        .split(',')
+        .map(
+          int.parse,
+        )
+        .reduce(
           (a, b) => a + b,
         );
   }
